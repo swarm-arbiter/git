fcedf0079de5cf89c8952fd0d50ae1f3b4b9b29f
Jamie Terry <jnterry@ntlworld.com>
2018-06-07 12:36:58 +0100

    Add status print on container launch

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,8 +3,18 @@
 install -d -D -o git -g git -m 0660 /run/lighttpd
 install -d -D -o git -g git -m 0774 /var/log/lighttpd
 
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
-
-exec /bin/sh
+printf "\n\n"
+printf "+===========================================\n"
+printf "| Completed cgit setup\n"
+printf "+-------------------------------------------\n"
+printf "| Container Version : "
+cat /VERSION
+printf "\n"
+printf "| cgit version      : "
+apk info cgit 2> /dev/null | grep description | awk '{print $1;}'
+printf "| Hostname          : $HOSTNAME\n"
+printf "| Local ip          : $(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')\n"
+printf "+===========================================\n"
+printf "\n\n"
 
 exec /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D
